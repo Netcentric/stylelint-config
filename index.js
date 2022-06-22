81d6ee677a847fc666bbe2bd0e0e7f6fa1e1ba91
Juan Hernando <juan.hernando@netcentric.biz>
2022-06-22 10:43:58 +0200

    Activate resolveNestedSelectors and add new test cases

--- index.js
+++ index.js
@@ -51,9 +51,10 @@ module.exports = {
     // bem
     // https://github.com/postcss/postcss-bem-linter/issues/82#issuecomment-193851353
     "selector-class-pattern": [
-      "^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$",
+      "^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*){0,2}(?:\\[.+\\])?$",
       {
-        "message": "Expected class selector to match BEM naming convention"
+        "message": "Expected class selector to match BEM naming convention",
+        "resolveNestedSelectors": true
       }
     ]
   }
